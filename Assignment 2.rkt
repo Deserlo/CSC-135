df69ed25bca2c94fedf6a7f8e3d65124d462bc4e
Deserlo <35828835+Deserlo@users.noreply.github.com>
2018-11-07 15:04:26 -0800

    make6

--- Assignment 2.rkt
+++ Assignment 2.rkt
@@ -5,6 +5,38 @@
    Evamarie Najera
 |#
 
+
+;1.) make 6
+
+(define ( toString str )
+   (number->string str))
+
+( define (checkNegative num)
+(string-prefix? (toString num) "-"))
+
+(define ( checkLength a )
+  (if (> 3 ( length a )) #f #t ))
+
+(define ( length num )
+  (if (checkNegative num) (- (string-length (toString num)) 1) (string-length(toString num))))
+
+(define (first a )
+  (substring (toString a) 0 3))
+
+(define (last b)
+   (substring (toString b) (- (string-length(toString b)) 3) (string-length(toString b))))
+                         
+( define ( make6  x y )
+   ( if (checkLength x) (if (checkLength y) (string-append (first x) (last y) ) "-2") "-2" ) )
+
+(make6 123 479)
+(make6 -12 456)
+(make6 1 9)
+(make6 123 12)
+(make6 123457 -453)
+
+
+
 #|-----Function3------|#
  (define(buildList N E1)
     (if (= N 0) '()
