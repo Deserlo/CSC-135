91a915d7b51ca64d66da22fc72032ed5fad10a3f
Deserlo <35828835+Deserlo@users.noreply.github.com>
2018-11-07 15:09:03 -0800

    concat

--- Assignment 2.rkt
+++ Assignment 2.rkt
@@ -35,6 +35,15 @@
 (make6 123 12)
 (make6 123457 -453)
 
+;2.) concat
+
+; needs tweaking
+; gives '((a b) (f f f) (c d) (d e s) (d e) (v v v v)) as result
+(define (concatMe L M list-so-far)
+  (if (null? L) list-so-far
+      (concatMe (cdr L)(cdr M)(append list-so-far(list(car L)(car M))))))
+  
+(concatMe '( (a b) (c d) (d e) ) '( (f f f) (d e s) (v v v v) ) '() )
 
 
 #|-----Function3------|#
